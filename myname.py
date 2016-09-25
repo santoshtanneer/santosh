ed0a200d80c5f8c8035b3a5d4137faf15e03ee8e
santu <tanneerkumar@gmail.com>
2016-09-24 20:18:22 -0700

    mychanges

--- myname.py
+++ myname.py
@@ -1,1 +1,26 @@
-print "Myname"
+hello world
+
+jhd
+hiodvhd
+]bsdv
+bosdvbsv
+]bsdvdovnsd
+bvodvbdv]uefgevbv'vkb
+bvsdkv
+dvbdvd\afhdv
+dvb
+vbsdvoidhv
+clihv
+dd pvishs
+dvbdslvihsvs
+vvnvihsd
+vsdvdvpdv
+ddnvsihv
+vdvs bsv
+]vsvbsv
+svbsv
+svofhb/v;lsdvoh
+dnvsvhsv
+]nvohvfl
+efjoiefhefsfv ovjws\vbw
+v
